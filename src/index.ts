1cced41a73a57695fa385206ab30f3122e5fc1a7
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-06-05 19:46:56 -0300

    updating index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import { program } from 'commander';
-import { setupProject, addScripts } from './lib';
+import { setupProject, addScripts, createConfigFiles } from './lib';
 import { updateChangelog } from './lib/update-changelog';
 
 program
-  .command('setup-project')
-  .option(
-    '-f, --force',
-    'force overwriting of config files and package scripts',
+  .command(
+    'setup-project',
+    'It will create basic config files and update your package scripts',
   )
+  .option('-f, --force', 'force overwriting')
   .action(setupProject);
 program
-  .command('add-scripts')
-  .option('-f, --force', 'force overwriting of package scripts')
+  .command('add-scripts', 'It will update your package scripts')
+  .option('-f, --force', 'force overwriting')
   .action(addScripts);
+program
+  .command('create-config-files', 'It will create config files')
+  .option('-f, --force', 'force overwriting')
+  .action(createConfigFiles);
 program.command('update-changelog').action(updateChangelog);
 
 program.parse(process.argv);
